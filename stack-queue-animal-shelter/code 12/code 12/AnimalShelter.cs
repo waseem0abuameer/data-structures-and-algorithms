2488d2860cc5841553832147cb14892197358dac
waseem0abuameer <waseem.a.abuameer@gmail.com>
2022-04-19 06:25:16 +0300

    add anmail

---
+++ stack-queue-animal-shelter/code 12/code 12/AnimalShelter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace code_12
+{
+    public class AnimalShelter
+    {
+        public Queue<Dog> Dogs { get; set; }
+        public Queue<Cat> Cats { get; set; }
+
+        public AnimalShelter()
+        {
+            Dogs = new Queue<Dog>();
+            Cats = new Queue<Cat>();
+        }
+
+        public void Enqueue(Animal animal)
+        {
+            if (animal is Cat)
+            {
+                Cat cat = (Cat)animal;
+                Cats.Enqueue(cat);
+            }
+            else if (animal is Dog)
+            {
+               
+            }
+        }
+        /*public Animal Dequeue(string pref)
+        {
+            
+        }*/
+    }
+}
---
+++ stack-queue-animal-shelter/code 12/code 12/Cat.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace code_12
+{
+    public class Cat : Animal
+
+    {
+        
+    }
+}
