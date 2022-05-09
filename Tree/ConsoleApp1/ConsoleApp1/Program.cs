8f9c49699afa85cca3d535d476645062337c8a82
waseem abuameer <72654012+waseem0abuameer@users.noreply.github.com>
2022-05-09 06:06:53 +0300

    Update Program.cs

--- Tree/ConsoleApp1/ConsoleApp1/Program.cs
+++ Tree/ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,22 +6,18 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            BinaryTree binaryTree = new BinaryTree(new Node(10));
-            binaryTree.Root.Left = new Node(8);
-            binaryTree.Root.Right = new Node(15);
-            binaryTree.Root.Left.Left = new Node(5);
-            binaryTree.Root.Right.Right = new Node(20);
-            Console.WriteLine(binaryTree.ToString(binaryTree.PreOrder(binaryTree.Root)));
-            Console.WriteLine(binaryTree.ToString(binaryTree.PostOrder(binaryTree.Root)));
-            Console.WriteLine(binaryTree.ToString(binaryTree.InOrder(binaryTree.Root)));
-            BinarySearchTree binarySearchTree = new BinarySearchTree(new Node(10));
-            binarySearchTree.Add(8);
-            binarySearchTree.Add(15);
+                        BinarySearchTree binarySearchTree = new BinarySearchTree(new Node(10));
+            binarySearchTree.Add(2);
+            binarySearchTree.Add(7);
+            binarySearchTree.Add(2);
+            binarySearchTree.Add(6);
             binarySearchTree.Add(5);
-            binarySearchTree.Add(20);
-            Console.WriteLine(binarySearchTree.Contains(binaryTree.Root, 7));
+            binarySearchTree.Add(5);
+            binarySearchTree.Add(11);
+            binarySearchTree.Add(9);
+            binarySearchTree.Add(4);
+
             Console.WriteLine(binarySearchTree.ToString(binarySearchTree.InOrder(binarySearchTree.Root)));
-     
         }
     }
 }
