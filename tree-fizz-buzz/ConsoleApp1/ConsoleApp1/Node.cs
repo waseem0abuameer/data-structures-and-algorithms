42bb2de16d4d1774fee340a9b0e2680d37d426e9
waseem0abuameer <waseem.a.abuameer@gmail.com>
2022-05-09 06:38:48 +0300

    add Lab 18 solve

---
+++ tree-fizz-buzz/ConsoleApp1/ConsoleApp1/Node.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Node
+    {
+        public int Value { get; set; }
+        public Node Left { get; set; }
+        public Node Right { get; set; }
+        public Node(int value, Node left = null, Node right = null)
+        {
+            Value = value;
+            Left = left;
+            Right = right;
+        }
+    }
+}
---
+++ tree-fizz-buzz/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //BinarySearchTree binarySearchTree = new BinarySearchTree(new Node(10));
+            //binarySearchTree.Add(2);
+            //binarySearchTree.Add(7);
+            //binarySearchTree.Add(2);
+            //binarySearchTree.Add(6);
+            //binarySearchTree.Add(5);
+            //binarySearchTree.Add(5);
+            //binarySearchTree.Add(11);
+            //binarySearchTree.Add(9);
+            //binarySearchTree.Add(4);
+
+            //Console.WriteLine(binarySearchTree.ToString(binarySearchTree.InOrder(binarySearchTree.Root)));
+            //Console.WriteLine(binarySearchTree.MaxTree());
+
+            //BinarySearchTree tree = new BinarySearchTree();
+            //tree.Add(2);
+            //tree.Add(7);
+            //tree.Add(2); 
+            //tree.Add(6);
+            //tree.Add(5);
+            //tree.Add(11); 
+            //tree.Add(9); 
+            //tree.Add(4);
+            //Console.WriteLine(tree.ToString(tree.BreadthFirstTree(tree.Root)));
+
+            FizzBuzzTree root = new FizzBuzzTree("1");
+            root.Children.Add(new FizzBuzzTree("3"));
+            root.Children.Add(new FizzBuzzTree("5"));
+
+            root.Children[0].Children.Add(new FizzBuzzTree("15"));
+            root.Children[0].Children.Add(new FizzBuzzTree("215"));
+            root.Children[0].Children.Add(new FizzBuzzTree("360"));
+            root.Children[1].Children.Add(new FizzBuzzTree("7"));
+            
+
+          
+
+            List<string> list2 = root.FizzBuzz(root);
+            foreach (string s in list2)
+            {
+                Console.WriteLine(s);
+            }
+
+        }
+    }
+}
