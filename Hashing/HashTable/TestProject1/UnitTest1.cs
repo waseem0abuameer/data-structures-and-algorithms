d8b00baeebc1859b30f92d2cc12ccc447e01249d
waseem0abuameer <waseem.a.abuameer@gmail.com>
2022-06-14 03:34:16 +0300

    add test

--- Hashing/HashTable/TestProject1/UnitTest1.cs
+++ Hashing/HashTable/TestProject1/UnitTest1.cs
@@ -1,10 +1,57 @@
 using System;
 using Xunit;
 using HashTable;
+using HashTable;
 namespace TestProject1
 {
     public class UnitTest1
     {
-        HashTable testHashTable = new HashTable();
+        [Fact]
+        public void TestOne()
+        {
+            string s1 = "to negotiate contract labor for the Kingdom of Hawaii's sugar plantations, with hopes of saving the dwindling";
+
+            Assert.Equal("of", Program.RepeatedWord(s1));
+        }
+
+        [Fact]
+        public void TestTwo()
+        {
+            string s2 = " that the acquisition of land for the Utah resort hotel Amangiri (pictured) required an act of Congress?";
+
+            Assert.Equal("the", Program.RepeatedWord(s2));
+        }
+
+        [Fact]
+        public void TestThree()
+        {
+            string s3 = "commemorated a century later a with ";
+
+            Assert.NotEqual("b", Program.RepeatedWord(s3));
+        }
+
+        [Fact]
+        public void testcode301()
+        {
+            HashTable.HashTable testHashtable = new HashTable.HashTable();
+
+            testHashtable.Set("100", "Ali");
+
+            Assert.True(testHashtable.Contains("100"));
+        }
+
+        [Fact]
+        public void testcode302()
+        {
+            HashTable.HashTable testHashtable = new HashTable.HashTable();
+
+            testHashtable.Set("1", "waseem");
+
+            Assert.Equal("waseem", testHashtable.Get("1").Value);
+        }
+
+     
+     
+
     }
 }
